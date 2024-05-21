083aae5bdb0435df877ae49d85c967238f958cd7
admin <admin@DESKTOP-GVIRPN1>
2024-05-21 17:37:47 +0530

    Niveth Balaji

---
+++ TestNG/src/test/java/TestNG/TestcaseTest.java
@@ -0,0 +1,13 @@
+package TestNG;
+
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+public class TestcaseTest {
+	@Test
+	public void test()
+	{
+		Reporter.log("Nikitaba",true);
+		
+	}
+}
