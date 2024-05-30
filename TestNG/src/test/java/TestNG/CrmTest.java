b8e8cb25d086fd095b1152f137ca411182a7b9ad
admin <admin@DESKTOP-GVIRPN1.mshome.net>
2024-05-30 12:42:03 +0530

    Browser added

--- TestNG/src/test/java/TestNG/CrmTest.java
+++ TestNG/src/test/java/TestNG/CrmTest.java
@@ -4,6 +4,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Reporter;
 import org.testng.annotations.Test;
 
 import fileUtility.File_Utility;
@@ -16,8 +18,11 @@ public class CrmTest {
 		String Browser = System.getProperty("Browser");
 		if(Browser.equalsIgnoreCase("chrome")) {
 			driver= new ChromeDriver();
-		}else {
+		}else if(Browser.equalsIgnoreCase("edge")) {
 			driver = new EdgeDriver();
+		}
+		else {
+			driver= new FirefoxDriver();
 		}
 		String URL = System.getProperty("url");
 		String UN = System.getProperty("username");
@@ -26,6 +31,7 @@ public class CrmTest {
 		driver.findElement(By.name("user_name")).sendKeys(UN);
 		driver.findElement(By.name("user_password")).sendKeys(PW);
 		driver.findElement(By.id("submitButton")).click();
+		Reporter.log("Crm lunched",true);
 		driver.quit();
 		
 		
